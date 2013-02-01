c3d36e9e23e9a532a6affbb45a19cecdea6d8b45
liuwei <liuwei33@gmail.com>
2013-02-01 08:14:01 +0800

    new

---
+++ packages/three/package.js
@@ -0,0 +1,14 @@
+  
+
+
+Package.describe({
+  summary: "three.js - lightweight 3D library with a very low level of complexity"
+});
+
+Package.on_use(function (api) {
+  api.add_files('three.js', 'client');
+  api.add_files('TrackballControls.js', 'client');
+  api.add_files('THREEx.FullScreen.js', 'client');
+  api.add_files('THREEx.screenshot.js', 'client');
+});
+
